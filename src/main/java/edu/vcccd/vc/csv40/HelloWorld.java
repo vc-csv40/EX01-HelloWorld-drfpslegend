1f3aed3324457e42af28e7c698c7dcb00050ae52
drfpslegend <kevin.holst14@gmail.com>
2020-01-15 14:37:02 -0800

    attempt number tree

--- src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -7,10 +7,10 @@ package edu.vcccd.vc.csv40;
  * Statement of code ownership: I hereby state that I have written all of this
  * code and I have not copied this code from any other person or source.
  *
- * @author [CHANGE THIS TO YOUR INFORMATION]
+ * @author Kevin Holst (drfpslegend), kevin_holst1@my.vcccd.edu
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World!\n");
     }
 }
